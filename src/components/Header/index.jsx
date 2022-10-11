c5a89bb33540483781f49a64fb64c35608d03515
ggernigon <ggernigon@puydufou.com>
2022-10-11 15:25:48 +0200

    finish header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,23 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
+import "./style/Header.css";
 
 export default function Header() {
   return (
     <header>
-      <img src={logo} alt="logo de Kasa" />
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Accueil</Link>
-          </li>
-          <li>
-            <Link to="/about">A propos</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="header">
+        <img className="logo" src={logo} alt="logo de Kasa" />
+        <nav>
+          <ul className="menu">
+            <li>
+              <Link to="/">Accueil</Link>
+            </li>
+            <li>
+              <Link to="/about">A propos</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./routes/root";
+import { RouterProvider } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
 import Error from "./pages/Error/index";
 import About from "./pages/About";
 import Home from "./pages/Home/Index";
+import { createBrowserRouter } from "react-router-dom";
+import "./styles/index.css";
 
 const router = createBrowserRouter([
   {
@@ -30,3 +33,12 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+function Root() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <Outlet />
+    </div>
+  );
+}
