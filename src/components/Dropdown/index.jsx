7d8a3ef87ab672ee40275879aa893732ec07f42b
ggernigon <ggernigon@puydufou.com>
2022-11-28 15:16:38 +0100

    dropdown

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -6,14 +6,13 @@ export default function Dropdown({ title, informations }) {
 
   return (
     <div className="dropdown">
-      <div className="dropdown-header">
+      <div
+        className="dropdown-header"
+        onClick={isOpen ? () => setIsOpen(false) : () => setIsOpen(true)}
+      >
         <div className="dropdown-header__title">{title}</div>
         <div className="dropdown-header__arrow rotate">
-          {isOpen ? (
-            <ChevronUp onClick={() => setIsOpen(false)} />
-          ) : (
-            <ChevronDown onClick={() => setIsOpen(true)} />
-          )}
+          {isOpen ? <ChevronUp /> : <ChevronDown />}
         </div>
       </div>
       <div className={isOpen ? "content" : "close"}>
